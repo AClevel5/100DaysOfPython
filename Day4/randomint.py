ed476526728ec3eda511c8e7b98cf420af8cc1b3
Alex Cleveland <Alexcleveland5@gmail.com>
2023-10-03 19:01:30 -0600

    random function for ints and floats

---
+++ Day4/randomint.py
@@ -0,0 +1,14 @@
+import random
+
+
+# random int
+random_int = random.randint(0, 100)
+print(random_int)
+
+# random float
+random_float = random.random()
+print(random_float)
+
+# float between 0-99
+random_big_float = 99 * random_float
+print(random_big_float)
