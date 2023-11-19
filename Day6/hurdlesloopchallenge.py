1c794ce4381f392e541b14288b3bbcad2a2c2cee
Alex Cleveland <Alexcleveland5@gmail.com>
2023-11-19 07:53:24 -0700

    Reboorgs World function activity

---
+++ Day6/hurdlesloopchallenge.py
@@ -0,0 +1,16 @@
+# Reeborgs world moving the robot to end point will not work in terminal.
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def jump_hurdle():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
