1e7031ed628477eb6ccdbe6fdcc187a3e4ec6799
Alex Cleveland <Alexcleveland5@gmail.com>
2023-11-23 09:36:27 -0700

    Determine win or loss

--- Day7/hangman.py
+++ Day7/hangman.py
@@ -5,14 +5,23 @@ random_word = random.choice(word_list)
 print(random_word)
 display = []
 word_length = len(random_word)
+game_over = False
+counter = 0
 
 for _ in range(word_length):
     display += "_"
 
-chosen_letter = input("Choose a letter ").lower()
-for position in range(word_length):
-    letter = random_word[position]
-    if letter == chosen_letter:
-        display[position] = letter
-
-print(display)
+while game_over == False:
+    chosen_letter = input("Choose a letter ").lower()
+    counter += 1
+    for position in range(word_length):
+        letter = random_word[position]
+        if letter == chosen_letter:
+            display[position] = letter
+            print(display)
+    if "_" not in display:
+        game_over = True
+        print("WINNA")
+    if counter == 6:
+        game_over = True
+        print("Loser")
