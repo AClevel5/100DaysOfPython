ea2b77c6636e52855c4c6ba7401f382d0418259b
Alex Cleveland <Alexcleveland5@gmail.com>
2023-11-23 08:32:27 -0700

    get input from user

--- Day7/hangman.py
+++ Day7/hangman.py
@@ -5,5 +5,6 @@ word_list = ["aardvark", "baboon", "camel", "dog", "trout"]
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 random_word = random.choice(word_list)
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+chosen_letter = input("Choose a letter ").lower()
 
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
