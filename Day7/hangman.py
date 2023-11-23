724bff247de15ed5de098e43432bf800984d6fa3
Alex Cleveland <Alexcleveland5@gmail.com>
2023-11-23 09:17:25 -0700

    appending list of guesses

--- Day7/hangman.py
+++ Day7/hangman.py
@@ -3,14 +3,16 @@ import random
 word_list = ["aardvark", "baboon", "camel", "dog", "trout"]
 random_word = random.choice(word_list)
 print(random_word)
-chosen_letter = input("Choose a letter ").lower()
 display = []
+word_length = len(random_word)
 
-for x in random_word:
-    display.append("_")
+for _ in range(word_length):
+    display += "_"
 
-for letter in random_word:
+chosen_letter = input("Choose a letter ").lower()
+for position in range(word_length):
+    letter = random_word[position]
     if letter == chosen_letter:
-        print("Right")
-    else:
-        print("Wrong")
+        display[position] = letter
+
+print(display)
