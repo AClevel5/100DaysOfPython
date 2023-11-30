8ba44f5c7d72af91888de911d79b8c947ca5a85e
Alex Cleveland <Alexcleveland5@gmail.com>
2023-11-29 20:09:06 -0700

    decode

--- Day8/caesarcipher.py
+++ Day8/caesarcipher.py
@@ -42,3 +42,15 @@ def encrypt(plain_text, shift_amount):
         new_letter = alphabet[new_position]
         cipher_text += new_letter
     print(cipher_text)
+
+
+def decode(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        if new_position < 0:
+            new_position = new_position + 26
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+    print(cipher_text)
