9ca98df6e3e3468be6a1120b43606304be88e669
Alex Cleveland <Alexcleveland5@gmail.com>
2023-11-29 19:25:58 -0700

    Paint Calc Func

--- Day8/paintareacalc.py
+++ Day8/paintareacalc.py
@@ -4,8 +4,11 @@ h = int(input("What is the height of the walls? "))
 w = int(input("what is the width of the walls? "))
 c = 5
 
-num_of_cans = (h * w) / c
 
-round_num = math.ceil(num_of_cans)
+def paint_calc(h, w, c):
+    num_of_cans = (h * w) / c
+    round_num = math.ceil(num_of_cans)
+    print(f"You'll need {round_num} cans of paint")
 
-print(f"You'll need {round_num} cans of paint")
+
+paint_calc(h, w, c)
