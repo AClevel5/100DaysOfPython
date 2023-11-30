f91f5bd13efcb8c5288c624afcc3a9903cd4186c
Alex Cleveland <Alexcleveland5@gmail.com>
2023-11-29 20:18:26 -0700

    Caesar Cipher complete

--- Day8/caesarcipher.py
+++ Day8/caesarcipher.py
@@ -27,7 +27,7 @@ alphabet = [
     "z",
 ]
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'encrypt' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
